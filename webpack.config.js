79e987f24f75a44c9c9b2db9ad79c900ca382f2d
Andrei Kaminsky <andrei.kaminsky@incredibuild.com>
2020-11-16 19:23:47 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,17 @@ module.exports = {
                 }
             }
         }]
-    }
+    },
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.done.tap('DonePlugin', (stats) => {
+                    console.log('Compile is done !')
+                    setTimeout(() => {
+                        process.exit(0)
+                    })
+                });
+            }
+        }
+    ]
 }
