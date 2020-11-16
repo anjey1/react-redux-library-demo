d74d387046813361e8d3972751bca4887819666a
Andrei Kaminsky <andrei.kaminsky@incredibuild.com>
2020-11-16 19:51:25 +0200

    webpack update

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var express = require('express');
-var path = require('path');
-
-
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
 // Define Static Files Folder
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
 
 app.listen(3000, function () {
     console.log('app is listening on port 3000')
-})
+});
